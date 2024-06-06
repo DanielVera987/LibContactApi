3ab154241d45b0033b7f1e9c124ac631b08128cd
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-06-06 13:22:48 -0500

    Add missing fields to request update

--- app/Http/Requests/Contact/ContactUpdateRequest.php
+++ app/Http/Requests/Contact/ContactUpdateRequest.php
@@ -22,7 +22,9 @@ class ContactUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string:255'
+            'name' => 'required|string:255',
+            'last_name' => 'required',
+            'company' => ''
         ];
     }
 }
