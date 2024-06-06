708e30281728d75ea4f09ffcf5365244184444c8
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-06-06 11:05:13 -0500

    Add seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Contact::factory(15)->create();
+        \App\Models\ContactPhone::factory(10)->create();
+        \App\Models\ContactEmail::factory(14)->create();
+        \App\Models\ContactAddress::factory(15)->create();
     }
 }
